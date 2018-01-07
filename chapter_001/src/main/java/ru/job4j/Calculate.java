7b4b70e563fc464d67fb52d42662a029a0a53bd8
Ivan Salmin <salmin.iv@gmail.com>
2018-01-07 13:23:39 +0400

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,5 +1,10 @@
 package ru.job4j;
-
+/**
+* Конструктор, вывод строки на консоль.
+*@author isalmin
+*@since 01.07.2018
+*@param arg - arg.
+*/
 public class Calculate {
 	public static void main(String[] args) {
 		System.out.println("Hello world.");
---
+++ chapter_001/src/main/java/ru/job4j/package-info.java
@@ -0,0 +1,7 @@
+/**
+* Package for calculate task.
+* @author Ivan Salmin (salmin.iv@gmail.com)
+* @version $Id$
+* @since 01.07.2018
+*/
+ package ru.job4j;
