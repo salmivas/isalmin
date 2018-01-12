679b13bcb04e0d38a9a95905be13bc46208c1c43
Ivan Salmin <salmin.iv@gmail.com>
2018-01-12 19:21:50 +0400

    change of 1.3. JavaDoc. Code convention [#36419] in accordance with comments (add JavaDoc to method)

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -11,6 +11,7 @@ public class Calculate {
     /**
      * Main.
      * @param args - args.
+     * @return Hello World
      */
     public static void main(String[] args) {
         System.out.println("Hello World");
