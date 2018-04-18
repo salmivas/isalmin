a7636ea81b3d708e84086f88a85ef61b31722bf5
Ivan Salmin <salmin.iv@gmail.com>
2018-04-18 15:29:57 +0400

    change

--- isalmin/chapter_001/src/test/java/ru/job4j/condition/PointTest.java
+++ isalmin/chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -8,8 +8,8 @@ public class PointTest {
 
     @Test
     public void whenThen() {
-        Point a = new Point (0, 1);
-        Point b = new Point (2, 5);
+        Point a = new Point(0, 1);
+        Point b = new Point(2, 5);
         double result = b.distanceTo(a);
         assertThat(result, is(4.47213595499958));
     }
