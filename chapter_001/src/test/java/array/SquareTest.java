474f9a0910ea5c092f4cac42ba634f1f375f7902
Ivan Salmin <salmin.iv@gmail.com>
2018-05-09 17:49:38 +0500

    4.5. Рефакторинг кода. [#33459]

---
+++ chapter_001/src/test/java/array/SquareTest.java
@@ -0,0 +1,19 @@
+package array;
+
+import org.junit.Test;
+import ru.job4j.array.Square;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class SquareTest {
+
+    @Test
+    public void whenBound3Then149(){
+        int bound = 3;
+        Square square = new Square();
+        int[] rst = square.calculate(bound);
+        int[] except = new int[]{1,4,9};
+        assertThat(rst,is(except));
+    }
+}
