f67ed06c6a970fb5dff96ad806d1946f4c532d49
Ivan Salmin <salmin.iv@gmail.com>
2018-06-10 12:59:08 +0400

    5.2. Перевернуть массив. [#4441]

--- chapter_001/src/main/java/ru/job4j/array/Check.java
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -5,13 +5,13 @@ public class Check {
 
         boolean result = false;
 
-        for(int i = 0; i < data.length; i++) {
-                if (data[i] != data[i + 1]) {
-                    result = false;
-                    break;
-                } else {
-                    result = true;
-                }
+        for(int i = 0; i < data.length - 1; i++){
+            if (data[i] != data[i + 1]) {
+                result = false;
+                break;
+            } else {
+                result = true;
+            }
         }
         return result;
     }
--- chapter_001/src/test/java/ru/job4j/array/CheckTest.java
+++ chapter_001/src/test/java/ru/job4j/array/CheckTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.assertThat;
 
 public class CheckTest {
     @Test
-    public void whenDataMonoByTrueThenTrue() {
+    public void whenDataMonoByTrueThenTrueOdd() {
         Check check = new Check();
         boolean[] input = new boolean[] {true, true, true};
         boolean result = check.mono(input);
@@ -14,10 +14,24 @@ public class CheckTest {
     }
 
     @Test
-    public void whenDataNotMonoByTrueThenFalse() {
+    public void whenDataNotMonoByTrueThenFalseOdd() {
         Check check = new Check();
-        boolean[] input = new boolean[] {true, true, false};
+        boolean[] input = new boolean[] {true, false, true};
         boolean result = check.mono(input);
         assertThat(result, is(false));
+    }
+    @Test
+    public void whenDataNotMonoByTrueThenFalseEven() {
+        Check check = new Check();
+        boolean[] input = new boolean[] {true, true, true, false};
+        boolean result = check.mono(input);
+        assertThat(result, is(false));
+    }
+    @Test
+    public void whenDataNotMonoByTrueThenTrueEven() {
+        Check check = new Check();
+        boolean[] input = new boolean[] {true, true, true, true};
+        boolean result = check.mono(input);
+        assertThat(result, is(true));
     }
 }
