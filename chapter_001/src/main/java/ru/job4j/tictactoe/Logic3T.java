82c1a3504c894cd1a7aa8276015850f292fc25af
Ivan Salmin <salmin.iv@gmail.com>
2018-08-25 12:23:54 +0400

    Крестики-нолики на JavaFX [#49777]

--- chapter_001/src/main/java/ru/job4j/tictactoe/Logic3T.java
+++ chapter_001/src/main/java/ru/job4j/tictactoe/Logic3T.java
@@ -11,25 +11,29 @@ public class Logic3T {
 
     public boolean isWinnerX() {
         boolean retVal = false;
+        if (this.fillBy(Figure3T::hasMarkX, 0, 0, 1, 1)
+                || this.fillBy(Figure3T::hasMarkX, this.table.length - 1, 0, -1, 1)) {
+            retVal = true;
+        }
         for (int index = 0; index != this.table.length; index++) {
-                if (this.fillBy(Figure3T::hasMarkX, index, 0, 0, 1)
-                        || this.fillBy(Figure3T::hasMarkX, 0, index, 1, 0)
-                        || this.fillBy(Figure3T::hasMarkX, index, index, 1, 1)
-                        || this.fillBy(Figure3T::hasMarkX, this.table.length - 1 - index, index, -1, 1)) {
-                    retVal = true;
-                    break;
-                }
+            if (this.fillBy(Figure3T::hasMarkX, index, 0, 0, 1)
+                    || this.fillBy(Figure3T::hasMarkX, 0, index, 1, 0)) {
+                retVal = true;
+                break;
             }
+        }
         return retVal;
     }
 
     public boolean isWinnerO() {
         boolean retVal = false;
+        if (this.fillBy(Figure3T::hasMarkO, 0, 0, 1, 1)
+                || this.fillBy(Figure3T::hasMarkO, this.table.length - 1, 0, -1, 1)) {
+            retVal = true;
+        }
         for (int index = 0; index != this.table.length; index++) {
             if (this.fillBy(Figure3T::hasMarkO, index, 0, 0, 1)
-                    || this.fillBy(Figure3T::hasMarkO, 0, index, 1, 0)
-                    || this.fillBy(Figure3T::hasMarkO, index, index, 1, 1)
-                    || this.fillBy(Figure3T::hasMarkO, this.table.length - 1 - index, index, -1, 1)) {
+                    || this.fillBy(Figure3T::hasMarkO, 0, index, 1, 0)) {
                 retVal = true;
                 break;
             }
