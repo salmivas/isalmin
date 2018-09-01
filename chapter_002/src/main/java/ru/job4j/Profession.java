46091988dfd1c3fc1387383a3a1f20e34566eb2a
Ivan Salmin <salmin.iv@gmail.com>
2018-09-01 16:41:39 +0400

    Реализация профессий в коде [#6837]

--- chapter_002/src/main/java/ru/job4j/Profession.java
+++ chapter_002/src/main/java/ru/job4j/Profession.java
@@ -3,4 +3,20 @@ package ru.job4j;
 public class Profession {
     private String name;
     private String occupation;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
+
+    public String getOccupation() {
+        return occupation;
+    }
 }
---
+++ chapter_002/src/main/java/ru/job4j/Student.java
@@ -0,0 +1,5 @@
+package ru.job4j;
+
+public class Student {
+    private String name;
+}
