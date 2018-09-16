95210a3a3aad2beb89dead3cff09a26e84fa85cb
Ivan Salmin <salmin.iv@gmail.com>
2018-09-16 14:09:54 +0400

    Реализовать класс Tracker [#396]

--- chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,6 +1,5 @@
 package ru.job4j.tracker;
 
-import java.io.*;
 import java.util.Arrays;
 import java.util.UUID;
 
@@ -21,7 +20,6 @@ public class Tracker {
 
     /**
      * Реализует добавление новых заявок в хранилище.
-     *
      * @param item новая заявка.
      * @return item.
      */
@@ -33,7 +31,6 @@ public class Tracker {
 
     /**
      * Генерирует уникальный ключ для заявки.
-     *
      * @return уникальный ключ.
      */
     private String generateId() {
@@ -42,85 +39,76 @@ public class Tracker {
 
     /**
      * Ищет ячейку по id, присваивает найденной ячейке объект(меняет ячейку требуемой) принимаемый в аргументе.
-     *
      * @param id   искомая ячейка.
      * @param item ячейка которая должна заменить искомую.
      */
-    public void replace(String id, Item item) {
+    public boolean replace(String id, Item item) {
+        boolean check = false;
         for (int index = 0; index < items.length; index++) {
             if (this.items[index].getId().equals(id)) {
                 this.items[index] = item;
+                item.setId(id);
+                check = true;
                 break;
             }
         }
+        return check;
     }
 
     /**
      * Удаляет ячейку, найденную по уникальному строковому ключу.
-     *
      * @param id строковый ключ.
      */
-    public void delete(String id) {
+    public boolean delete(String id) {
+        boolean check = true;
         Item[] innerItems = this.items;
-        Item delItem = findBuild(id);
+        Item delItem = findById(id);
         int innerItemsLength = innerItems.length;
         int indexOfDelItem = Arrays.asList(this.items).indexOf(delItem);
         int numMoved = innerItemsLength - indexOfDelItem - 1;
-
-        System.arraycopy(innerItems, indexOfDelItem + 1, this.items, indexOfDelItem, numMoved);
-        innerItems[innerItemsLength - 1] = null;
+        if (delItem != null) {
+            System.arraycopy(innerItems, indexOfDelItem + 1, innerItems, indexOfDelItem, numMoved);
+        } else {
+            check = false;
+        }
+        return check;
     }
 
     /**
      * Ищет незаполненные ячейки. Возвращает массив без незаполненных ячеек.
-     *
      * @return массив без незаполненных (пустых) ячеек.
      */
     public Item[] findAll() {
-        Item[] lookedFor = this.items;
-        for (int i = 0; i < lookedFor.length; i++) {
-            if (lookedFor[i] == null) {
-            //Выполнить delete
-            }
-        }
-        return null;
+        return Arrays.copyOf(this.items, this.position);
     }
 
     /**
      * Ищет совпадения по заданному имени.
-     *
      * @param key искомое имя.
      * @return массив найденных совпадений.
      */
     public Item[] findByName(String key) {
         int numberOfFoundElements = 0;
-        for (int i = 0; i < this.items.length; i++) {
-            if (this.items[i].getName().equals(key)) {
-                numberOfFoundElements++;
+        Item[] resultArray = new Item[this.items.length];
+        for (int i  = 0; i < this.items.length; i++) {
+            if (this.items[i] != null && this.items[i].getName().equals(key)) {
+                resultArray[numberOfFoundElements++] = this.items[i];
             }
         }
-        Item[] resultArray = new Item[numberOfFoundElements];
-        for (int i = 0; i < this.items.length; i++) {
-            if (this.items[i].getName().equals(key)) {
-                resultArray[i] = this.items[i];
-            }
-        }
+        System.arraycopy(resultArray, 0, resultArray, 0, numberOfFoundElements);
         return resultArray;
     }
 
     /**
      * Производит поиск по уникальному строковому ключу ячейки и возвращает найденный элемент.
-     *
      * @param id строковый индекс ячейки.
      * @return найденный элемент.
      */
-    public Item findBuild(String id) {
+    public Item findById(String id) {
         Item reqItem = null;
         for (int index = 0; index < this.items.length; index++) {
             if (this.items[index].getId().equals(id)) {
                 reqItem = this.items[index];
-                break;
-            } else {
                 break;
             }
         }
--- chapter_002/src/test/java/ru/job4j/tracker/TrackerTest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -9,6 +9,16 @@ import static org.junit.Assert.assertThat;
 
 public class TrackerTest {
 
+    @Test
+    public void whenReplaceNameThenReturnName() {
+        Tracker tracker = new Tracker();
+        Item previous = new Item("test1","testDescription",123L);
+        tracker.add(previous);
+        Item next = new Item("test2", "TestDescription2", 1234L);
+        next.setId(previous.getId());
+        tracker.replace(previous.getId(), next);
+        assertThat(tracker.findById(previous.getId()).getName(), is("test2"));
+    }
 
     @Test
     public void deleteItem() {
@@ -20,18 +30,7 @@ public class TrackerTest {
         tracker.add(secondItem);
         tracker.add(thirdItem);
         tracker.delete(secondItem.getId());
-        assertThat(tracker.findBuild(thirdItem.getId()), is("test3"));
-    }
-
-    @Test
-    public void whenReplaceNameThenReturnName() {
-        Tracker tracker = new Tracker();
-        Item previous = new Item("test1","testDescription",123L);
-        tracker.add(previous);
-        Item next = new Item("test2", "TestDescription2", 1234L);
-        next.setId(previous.getId());
-        tracker.replace(previous.getId(), next);
-        assertThat(tracker.findBuild(previous.getId()).getName(), is("test2"));
+        assertThat(tracker.findById(thirdItem.getId()), is(thirdItem));
     }
 
     @Test
@@ -44,5 +43,17 @@ public class TrackerTest {
         tracker.add(secondItem);
         tracker.add(thirdItem);
         assertThat(tracker.findByName(thirdItem.getName()), is(thirdItem));
+    }
+
+    @Test
+    public void findNameById() {
+        Tracker tracker = new Tracker();
+        Item firstItem = new Item("test1","testDescription",123L);
+        Item secondItem = new Item("test2", "TestDescription2", 1234L);
+        Item thirdItem = new Item("test3", "TestDescription3", 12345L);
+        tracker.add(firstItem);
+        tracker.add(secondItem);
+        tracker.add(thirdItem);
+        assertThat(tracker.findById(thirdItem.getId()), is(thirdItem));
     }
 }
